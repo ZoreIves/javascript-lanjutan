98f72c38924914cf8f72045b84e34d0eff6c3f61
Catur Hidayat <c.meong@gmail.com>
2020-12-07 22:50:21 +0700

    hapus value awal array dgn fungsi shift

--- array-shift.js
+++ array-shift.js
@@ -1,6 +1,13 @@
 function panggilShift(){
-    var arry = [1,2,3,4,5]
-    console.log(arry);
+    var arry = ["bekasi", "jakarta", "Jogja", "klaten"]
+    console.log(arry); // hasil console.log masih utuh
+
+    // arry = arry.shift()
+    arry2 = arry.shift() // fungsi shift => untuk menghapus value array di awal
+
+    console.log(arry2); // Hasil console.log sudah terhapus
+    return arry
+
 }
 
-panggilShift()
+console.log(panggilShift());
