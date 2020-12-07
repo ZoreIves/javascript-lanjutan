bde73b5dfa1ed3a8e2659b487ba89e2d1d2e76be
Catur Hidayat <c.meong@gmail.com>
2020-12-07 23:03:35 +0700

    tambah data ke array mnggnkn fungsi push

---
+++ array-push.js
@@ -0,0 +1,11 @@
+function fungsiPush(){
+    var arry = ["bekasi", "jakarta", "Jogja", "klaten"]
+    console.log(arry);
+
+
+    var desaAsal = "Karang Asem"
+    arry.push(desaAsal) // Fungsi push untuk menambahkan data ke array
+    return arry;
+}
+
+console.log(fungsiPush());
