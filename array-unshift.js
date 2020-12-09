90fcd7a5cd9fe9b758c03da19febde45639948cd
Catur Hidayat <c.meong@gmail.com>
2020-12-09 21:23:30 +0700

    fungsi unshift untuk tambah data di depan

---
+++ array-unshift.js
@@ -0,0 +1,9 @@
+function fungsiUnshift(){
+    var arry = ["bekasi", "jakarta", "Jogja", "klaten"]
+    console.log(arry);
+
+    arry.unshift("karang asem", "-ke-")
+    return arry
+}
+
+console.log(fungsiUnshift());
