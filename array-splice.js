63005a9abcfafd3fbadb52155a20e745e59c8ea2
Catur Hidayat <c.meong@gmail.com>
2020-12-09 21:16:27 +0700

    mengapus data menggunakan fungsi splice

--- array-splice.js
+++ array-splice.js
@@ -2,7 +2,8 @@ function fungsiSplice(){
     var arry = ["bekasi", "jakarta", "Jogja", "klaten"]
     console.log(arry);
 
-    arry.splice(2,0, "karang asem")
+    // arry.splice(2,0, "karang asem") // Menambah data menggunakan splice sesuai lokasi yang dippilih
+    arry.splice(1,3) // Menghapus data pada lokasi yang ditentukan dan jumlahnya berapa
     return arry
 }
 
