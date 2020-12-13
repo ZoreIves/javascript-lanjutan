418af86e07d400e8c5994b823979f25eddf1e57f
Catur Hidayat <c.meong@gmail.com>
2020-12-13 12:43:39 +0700

    tugas 14 javascript

---
+++ tugas14.js
@@ -0,0 +1,16 @@
+/*
+buat variable untuk pembagian, 
+kemudian lakukan pembagian 37 dibagi tgl lahir
+tampilkan hasil pembagian
+bulatkan hasil pembagian menggunakan ceil, floor, round
+*/
+
+var x = 37
+var tgl = 24
+
+var hasil = x/tgl
+console.log(hasil);
+
+console.log(Math.ceil(hasil));
+console.log(Math.floor(hasil));
+console.log(Math.round(hasil));
