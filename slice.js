0e7faa6bcf0427c4994aaf78f7ac5c5bd67ed52f
Catur Hidayat <c.meong@gmail.com>
2020-12-20 20:04:27 +0700

    Fungsi slice, delete str pd variable, mirip substr

---
+++ slice.js
@@ -0,0 +1,8 @@
+function slice(){
+    var kalimat = "Belajar Javascript Lanjutan"
+    console.log(kalimat.slice(8));
+    console.log(kalimat.slice(8, 18));
+    console.log(kalimat.slice(-8));
+}
+
+slice()
