bb0864b668c63b0d9ea046d30dc916f8ccb9df8d
Catur Hidayat <c.meong@gmail.com>
2020-12-22 20:38:23 +0700

    mambandingan string pd var, apakah ada yg sama

---
+++ stringInclude.js
@@ -0,0 +1,7 @@
+function stringInclude(){
+    var dev = "Belajar Javascript Lanjutan di Niomic"
+    console.log(dev.includes("Javascript"));
+    console.log(dev.includes("script"));
+}
+
+stringInclude()
