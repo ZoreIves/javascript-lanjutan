a74bf3d9be0820dc05b1561ecb430a07d766dc2b
Catur Hidayat <c.meong@gmail.com>
2020-12-22 20:34:03 +0700

    menggabungkan string dgn concat

---
+++ stringConcat.js
@@ -0,0 +1,9 @@
+function stringConcat(){
+    var dev = "Belajar Javascript String"
+    var add = dev.concat(" Concat")
+    console.log(add);
+    console.log(add.concat(" di Niomic"));
+
+}
+
+stringConcat()
