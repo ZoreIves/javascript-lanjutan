d4deb47c82523fb42b41078d1f208d4a887ce8ce
Catur Hidayat <c.meong@gmail.com>
2020-12-23 22:34:01 +0700

    konversi ke string

---
+++ convertString.js
@@ -0,0 +1,13 @@
+function konversi(){
+    var dev = new String ("Belajar Javascript Lanjutan...")
+    console.log(typeof dev);
+    console.log(dev);
+    var kon = dev.toString()
+    console.log(kon.toString);
+    console.log(kon);
+    var kon2 = dev.valueOf()
+    console.log(typeof kon2);
+    console.log(kon2);
+}
+
+konversi()
