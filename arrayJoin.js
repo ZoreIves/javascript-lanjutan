6ebe7fe0a0cdaaea4091d1a5e31c83441670dfa6
Catur Hidayat <c.meong@gmail.com>
2020-12-30 20:00:12 +0700

    mengubah string menjadi array

---
+++ arrayJoin.js
@@ -0,0 +1,9 @@
+function arrayJoin(){
+    var kota = ["Bekasi", "Jakarta", "Jogja", "Klaten"]
+    console.log(kota);
+
+    var result = kota.join(", ")
+    return result
+}
+
+console.log(arrayJoin());
---
+++ arraySplit.js
@@ -0,0 +1,9 @@
+function arraySplit(){
+    var dev = "Belajar javascript lanjutan, array split"
+    // console.log(dev);
+
+    var result = dev.split(" ")
+    return result
+
+}
+console.log(arraySplit());
