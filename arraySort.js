aa44a7cc60605c6abc10bf774f5b27a1a612d23c
Catur Hidayat <c.meong@gmail.com>
2021-01-02 14:41:25 +0700

    mengurutkan nilai pada array

---
+++ arraySort.js
@@ -0,0 +1,10 @@
+function arrySort(){
+    let arry = ["Jakarta", "Jogja", "Klaten", "Bekasi"]
+    console.log(arry);
+    console.log("==========================");
+    arry = arry.sort();
+
+    return arry
+}
+
+console.log(arrySort());
