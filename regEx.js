1d297967a1cd07a328000f67550ca2c7f621e65c
Catur Hidayat <c.meong@gmail.com>
2021-01-10 20:29:41 +0700

    RegEx (Regular Expresion)

---
+++ regEx.js
@@ -0,0 +1,22 @@
+function fungsiRegex(){
+    // let a = "Belajar Javascript di niomic"
+    let b = "abcdefghijklmnopqrstuvwxyz"
+
+    return b.search(/r/)
+
+    // return a.search(/a/)
+    
+}
+
+function fungsiRegex2(){
+    let a = "Belajar Javascript di niomic"
+    // let b = "abcdefghijklmnopqrstuvwxyz"
+
+    // return b.search(/c/)
+
+    return a.search(/a/)
+    
+}
+
+console.log(fungsiRegex(), fungsiRegex2());
+
