9463d785f94823417986b94464d154e74a90972c
Catur Hidayat <c.meong@gmail.com>
2021-01-10 20:16:46 +0700

    konversi to string

---
+++ fungsiString.js
@@ -0,0 +1,23 @@
+function fungsiString(){
+    var x = 56345;
+    var y = 235.23;
+    var z = "Belajar Javascript";
+    var a = false;
+
+    hasil = String(x)
+    console.log(hasil);
+
+    hasil2 = String(y)
+    console.log(hasil2);
+
+    hasilA = hasil + hasil2
+    console.log(hasilA);
+
+    hasil3 = String(z)
+    console.log(hasil3);
+
+    hasil4  = String(a)
+    console.log(hasil4);
+}
+
+fungsiString()
