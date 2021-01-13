1df48d5d32dbdb33c48bf01313df3f2c8eed359f
Catur Hidayat <c.meong@gmail.com>
2021-01-13 19:07:13 +0700

    flag pada regEx

---
+++ flagRegEx.js
@@ -0,0 +1,9 @@
+function flag(){
+    let data = "Belajar Javascipt Lanjutan di niomic | Flag"
+
+    console.log(data.search(/J/));
+    console.log(data.search(/J/i));
+    console.log(data.search(/j/i));
+}
+
+flag()
