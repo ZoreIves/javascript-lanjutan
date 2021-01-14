541b6d5db96c0313731f796bdcdfea6421afe6f0
Catur Hidayat <c.meong@gmail.com>
2021-01-14 16:35:04 +0700

    Bilangan Ganjil menggunakan fungsi modulus

---
+++ bilanganGanjil.js
@@ -0,0 +1,12 @@
+function ganjil(){
+    let bilanganGanjil = []
+
+    for (let i = 1; i < 30; i++) {
+        if(i%2 != 0){
+            bilanganGanjil.push(i)
+        }
+    }
+    return bilanganGanjil
+}
+
+console.log(ganjil());
